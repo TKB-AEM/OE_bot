1ab19ef6baf827ffc4aa2f0041d2979e43ec99a2
TKB-AEM <aem.tsukuba@gmail.com>
2014-10-01 17:59:17 +0900

    生に対応

--- lib/card.rb
+++ lib/card.rb
@@ -5,37 +5,22 @@ require_relative "./function/function.rb"
 
 class Card
 
+  # カードのIDを返す
   def Card::idnum
-    card = new
-    counts = 0
-
+    pasori = Pasori.open
     loop do
       begin
-        Pasori.open {|pasori|
-          pasori.felica_polling {|felica|
-            system = felica.request_system
-            return card.dump_system_info(pasori, system)
-          }
-        }
+        felica = pasori.felica_polling(Felica::POLLING_ANY)
+        idm = felica.idm.unpack("C*").map{|c| sprintf("%02X",c)}.join.to_s
+        felica.close
+        pasori.close
+        return idm
         break
 
       rescue PasoriError
-        sleep 2
+        sleep 1
       end
-
     end
-  end
-
-  def dump_id(felica)
-    return felica.idm.unpack("C*").map{|c| sprintf("%02X", c)}.join
-  end
-
-  private :dump_id
-
-  def dump_system_info(pasori, system)
-    pasori.felica_polling(system[0]) {|felica|
-      return dump_id(felica)
-    }
   end
 
   # カードのIDを渡すとユーザーのidを返す
