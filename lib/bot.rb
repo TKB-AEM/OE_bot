3ddccf70f9ea85e047ecbef4f118dc07db719688
seinosuke <seinosuke.3606@gmail.com>
2014-10-16 22:16:09 +0900

    投稿できなくても諦めない

--- lib/bot.rb
+++ lib/bot.rb
@@ -52,26 +52,33 @@ class Bot
   # 通常の投稿
   def post(text = "",twitter_id:nil,status_id:nil)
     str_time = Time.now.strftime("[%Y-%m-%d %H:%M]")
+    try = 0
 
-    # 会話の返事
-    if status_id
-      rep_text = "@#{twitter_id} #{text}"
-      rep_text += "\n#{str_time}"
-      rep_text = self.chain_post(text,twitter_id:twitter_id,status_id:status_id) if rep_text.size > 140
-      @client.update(rep_text,{:in_reply_to_status_id => status_id})
-      puts "#{rep_text}\n\n"
+      begin
+        # 会話の返事
+        if status_id
+          rep_text = "@#{twitter_id} #{text}"
+          rep_text += "\n#{str_time}"
+          rep_text = self.chain_post(text,twitter_id:twitter_id,status_id:status_id) if rep_text.size > 140
+          @client.update(rep_text,{:in_reply_to_status_id => status_id})
+          puts "#{rep_text}\n\n"
 
-    # ただの投稿(twitter_id:nil)か会話の始まり
-    else
-      post_text = twitter_id ? "@#{twitter_id} #{text}" : text
-      post_text += "\n#{str_time}"
-      post_text = self.chain_post(text,twitter_id:twitter_id,status_id:status_id) if post_text.size > 140
-      @client.update(post_text)
-      puts "#{post_text}\n\n"
-    end
+        # ただの投稿(twitter_id:nil)か会話の始まり
+        else
+          post_text = twitter_id ? "@#{twitter_id} #{text}" : text
+          post_text += "\n#{str_time}"
+          post_text = self.chain_post(text,twitter_id:twitter_id,status_id:status_id) if post_text.size > 140
+          @client.update(post_text)
+          puts "#{post_text}\n\n"
+        end
 
-  rescue
-    logs "post error! #{$!.class}: #{$!}\n#{$@[0]}"
+      # Twitter::Error::RequestTimeout: exection expired
+      rescue Twitter::Error
+        try += 1
+        logs "#{try}回目のpost error #{$!.class}: #{$!}\n#{$@[0]}"
+        sleep 1
+        retry if try < 3
+      end
   end
 
 
@@ -83,18 +90,23 @@ class Bot
     # ↓”＠〜”と”str_time”19文字と”（続く）”4文字を除いた最終的にpostに返せる最大文字数で分割
     post_size = 140 - (twitter_id_size + 19 + 4)
     texts = over_text.scan(/.{1,#{post_size}}/m)
+    try = 0
 
-    0.upto(texts.size - 2) do |i|
-      texts[i] = twitter_id ? "@#{twitter_id} #{texts[i]}(続く)" : "#{texts[i]}(続く)"
-      @client.update(texts[i],{:in_reply_to_status_id => status_id})
-      puts "#{texts[i]}\n\n"
+    begin
+      0.upto(texts.size - 2) do |i|
+        texts[i] = twitter_id ? "@#{twitter_id} #{texts[i]}(続く)" : "#{texts[i]}(続く)"
+        @client.update(texts[i],{:in_reply_to_status_id => status_id})
+        puts "#{texts[i]}\n\n"
+      end
+    rescue
+      try += 1
+      logs "#{try}回目のchain_post error #{$!.class}: #{$!}\n#{$@[0]}"
+      sleep 1
+      retry if try < 3
     end
 
     str_time = Time.now.strftime("[%Y-%m-%d %H:%M]")
     return "@#{twitter_id} #{texts[texts.size - 1]}\n#{str_time}"
-
-  rescue
-    logs "chain_post error! #{$!.class}: #{$!}\n#{$@[0]}"
   end
 
 
@@ -107,5 +119,4 @@ class Bot
   rescue
     logs "fav error! #{$!.class}: #{$!}\n#{$@[0]}"
   end
-
 end
